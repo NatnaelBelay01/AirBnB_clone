bdbbc4080ee043a0d1218f702119bed572840ee9
NatnaelBelay01 <natnaelbelayteklemariam@gmail.com>
2022-09-04 09:50:19 -0700

    tests

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """a city class that inherits from Basemodel """
+from models.base_model import BaseModel
 
 
 class City(BaseModel):
--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A state class that inherits from base"""
-from model.base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class State(BaseModel):
